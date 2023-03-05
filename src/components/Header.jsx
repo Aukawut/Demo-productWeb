14ee5c160706b96f21b82da74f2270b8e78276ea
aukawut2022 <110217436+aukawut2022@users.noreply.github.com>
2023-03-05 10:12:00 +0700

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,6 @@ import { useLanguageStore } from '../store'
 const Header = () => {
   const [isL,setIsL] = useState(useLanguageStore((state) => state.language))
 console.log("Header",isL);
-useEffect(() => {
-  if(isL == null ||isL=="null"||isL == undefined||isL == 'undefined'){
-    localStorage.setItem("L","TH");
-  }
-  setIsL("TH")
-},[isL])
-
   return (
     <div>
         <img src={isL == "TH" ? picHeader :picHeader2} className={`${css.imgHeader}`}/>
